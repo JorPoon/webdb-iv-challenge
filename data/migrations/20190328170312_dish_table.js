d654e697a6af8c6d37fc0521bd003c36e208bdfb
Jor_Poon <jormpoon@gmail.com>
2019-03-28 17:21:09 -0400

    created dish table

--- data/migrations/20190328170312_dish_table.js
+++ data/migrations/20190328170312_dish_table.js
@@ -1,8 +1,19 @@
-
+// dish table for recipeBook database.
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable('dishes', function(tbl) {
+        //primary key (auto increments)
+        tbl.increment();
+
+        //names of the dishes - required - must be different
+        tbl
+         .string('name', 128)
+         .notNullable()
+         .unique();
+    })
   
 };
 
+// undo changes from up function
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('dishes');
 };
